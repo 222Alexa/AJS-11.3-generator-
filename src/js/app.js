8792e0c2bf440d0660b28a593818fbd0908af1f4
Alexandra Titova <auto-steklo@mail.ru>
2021-03-16 13:38:38 +0300

    '*generator'

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+import canIterate from './canIterate';
+
+const map = new Map();
+const set = new Set();
+const number = 10;
+const string = 'Netology';
+const nulle = null;
+
+const list = [
+  map,
+  set,
+  number,
+  string,
+  nulle,
+];
+
+list.forEach((item) => {
+  const answer = canIterate(item);
+  console.log(item);
+  console.log(answer);
+});
